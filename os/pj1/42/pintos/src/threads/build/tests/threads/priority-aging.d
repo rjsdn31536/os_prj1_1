cc920d93beebaa9e628f411166a68c1d6f22fd53
dddwsd <quswotmd5@naver.com>
2018-11-30 05:13:28 -0800

    thread

---
+++ os/pj1/42/pintos/src/threads/build/tests/threads/priority-aging.d
@@ -0,0 +1,6 @@
+tests/threads/priority-aging.o: ../../tests/threads/priority-aging.c \
+  ../../lib/stdio.h ../../lib/debug.h ../../lib/stdarg.h \
+  ../../lib/stdbool.h ../../lib/stddef.h ../../lib/stdint.h \
+  ../../lib/kernel/stdio.h ../../tests/threads/tests.h \
+  ../../threads/init.h ../../threads/thread.h ../../lib/kernel/list.h \
+  ../../threads/synch.h
---
+++ os/pj1/42/pintos/src/threads/build/tests/threads/priority-change-2.d
@@ -0,0 +1,6 @@
+tests/threads/priority-change-2.o:  \
+ ../../tests/threads/priority-change-2.c ../../lib/stdio.h \
+  ../../lib/debug.h ../../lib/stdarg.h ../../lib/stdbool.h \
+  ../../lib/stddef.h ../../lib/stdint.h ../../lib/kernel/stdio.h \
+  ../../tests/threads/tests.h ../../threads/init.h ../../threads/thread.h \
+  ../../lib/kernel/list.h ../../threads/synch.h
